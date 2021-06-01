e9d54d93ffb37a8b663e051c2b626911bc827d46
Forcer0625 <61505055+Forcer0625@users.noreply.github.com>
2021-06-01 17:14:39 +0800

    Andy commited

--- HW10/ShellSort.java
+++ HW10/ShellSort.java
@@ -1,14 +1,14 @@
 class ShellSort
 {
     final private static int[] gap_seq={19903198,8845866,3931496,1747331,7765915,345152,153401,68178,30301,13467,5985,2660,1182,525,233,103,46,20,9,4,1};
-    //Shellsort.sort(arr,0,arr.length);
+    //Shellsort.sort(a,0,a.length);
     public static void sort(int a[],int start,int end)
     {
         for (int gap : gap_seq)
         {
             for (int i = start+end ; i < end  ; i ++ )
             {
-                int temp = arr[i];
+                int temp = a[i];
                 int j;
                 for (j = i; j >= gap+start && a[j - gap] > temp; j -= gap)
                     a[j] = a[j - gap];
